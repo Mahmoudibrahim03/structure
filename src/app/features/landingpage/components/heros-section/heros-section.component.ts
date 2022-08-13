dd3a002d07d4c894af7ea9c65a8238abf7727228
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2022-08-13 03:54:31 +0200

    export data object to a seperated JSON file

--- src/app/features/landingpage/components/heros-section/heros-section.component.ts
+++ src/app/features/landingpage/components/heros-section/heros-section.component.ts
@@ -1,80 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IActor } from 'src/app/core/interfaces/actors';
-
+import actors from '../../../../../assets/data/actors.json';
 @Component({
    selector: 'app-heros-section',
    templateUrl: './heros-section.component.html',
    styleUrls: ['./heros-section.component.scss']
 })
 export class HerosSectionComponent implements OnInit {
-   public actors: IActor[] = [
-      {
-         name: 'John Doe',
-         photo: 'assets/actors/actor1.jpeg',
-         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi.',
-         socialMedia: {
-            facebook: 'https://www.facebook.com/',
-            twitter: 'https://www.twitter.com/',
-            instagram: 'https://www.instagram.com/',
-         },
-         moreLink: 'https://www.google.com/',
-      },
-      {
-         name: 'Alex Doe',
-         photo: 'assets/actors/actor2.jpeg',
-         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi.',
-         socialMedia: {
-            facebook: 'https://www.facebook.com/',
-            twitter: 'https://www.twitter.com/',
-            instagram: 'https://www.instagram.com/',
-         },
-         moreLink: 'https://www.google.com/',
-      },
-      {
-         name: 'Alex Doe',
-         photo: 'assets/actors/actor3.jpeg',
-         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi.',
-         socialMedia: {
-            facebook: 'https://www.facebook.com/',
-            twitter: 'https://www.twitter.com/',
-            instagram: 'https://www.instagram.com/',
-         },
-         moreLink: 'https://www.google.com/',
-      },
-      {
-         name: 'Alex Doe',
-         photo: 'assets/actors/actor4.jpeg',
-         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi.',
-         socialMedia: {
-            facebook: 'https://www.facebook.com/',
-            twitter: 'https://www.twitter.com/',
-            instagram: 'https://www.instagram.com/',
-         },
-         moreLink: 'https://www.google.com/',
-      },
-      {
-         name: 'Alex Doe',
-         photo: 'assets/actors/actor5.jpeg',
-         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi.',
-         socialMedia: {
-            facebook: 'https://www.facebook.com/',
-            twitter: 'https://www.twitter.com/',
-            instagram: 'https://www.instagram.com/',
-         },
-         moreLink: 'https://www.google.com/',
-      },
-      {
-         name: 'Alex Doe',
-         photo: 'assets/actors/actor6.jpeg',
-         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi. Nam euismod, nisi eu consectetur consectetur, nisi nisi consectetur nisi, euismod nisi nisi nisi euismod nisi.',
-         socialMedia: {
-            facebook: 'https://www.facebook.com/',
-            twitter: 'https://www.twitter.com/',
-            instagram: 'https://www.instagram.com/',
-         },
-         moreLink: 'https://www.google.com/',
-      }
-   ];
+   public actors: IActor[] = actors;
 
    //eslint-disable-next-line @typescript-eslint/no-empty-function
    constructor() { }
